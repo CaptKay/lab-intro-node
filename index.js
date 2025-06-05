6da858b237ce102dab9148e714d7fadb3a5c7f78
Kanu Kingsley <suge.kanu@gmail.com>
2025-06-06 00:25:21 +0200

    lab solved

--- index.js
+++ index.js
@@ -1,17 +1,51 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos < 0 || pos >= this.items.length) {
+      throw new Error("OutOfBounds");
+    }
+    return this.items[pos];
+  }
 
-  max() {}
+  max() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    // return Math.max(...this.items)
+    const max = this.items.reduce((a, b) => Math.max(a, b), -Infinity);
+    return max;
+  }
 
-  min() {}
+  min() {
+       if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    // return Math.min(...this.items) //Not  good for an array with too many  elements
+    return this.items.reduce((a, b) => Math.min(a, b), Infinity);
+  }
 
-  sum() {}
+  sum() {
+    return this.items.reduce((acc , currVal) => acc + currVal, 0)
+  }
 
-  avg() {}
+  avg() {
+     if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+const sum = this.items.reduce((acc, currVal) => acc + currVal, 0)
+return sum/this.items.length
+
+  }
 }
 
 module.exports = SortedList;
